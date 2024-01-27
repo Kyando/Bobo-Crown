dbb2109b5324323d76346a917e288238fefb4866
Bruno Ribeiro <Brunoribeiro47@hotmail.com>
2024-01-27 13:29:21 -0300

    added tiger
    added tiger enemy and changed physics

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 public class BasicEnemy : MonoBehaviour
 {
-    
+    public List<Transform> pointsOfInterest;
+    private Transform _targetPointOfInterest;
+
+
+    private void Awake()
+    {
+        SelectRandomPointOfInterest();
+    }
+
+    private void SelectRandomPointOfInterest()
+    {
+        Transform nextPoint = _targetPointOfInterest;
+        do
+        {
+            int pointIndex = Random.Range(0, pointsOfInterest.Count);
+            nextPoint = pointsOfInterest[pointIndex];
+        } while (nextPoint == _targetPointOfInterest);
+
+        _targetPointOfInterest = nextPoint;
+    }
+
+    private void Update()
+    {
+        
+    }
 }
