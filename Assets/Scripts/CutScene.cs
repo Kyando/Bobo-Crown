faf47123ad2a950c021c845a67d85c5aad9fe917
Willian Henkel <willianhenkel@gmail.com>
2024-01-27 21:07:03 -0300

    added ui

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+    public int screenPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
