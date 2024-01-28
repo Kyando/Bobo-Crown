1c6d6cce4128a34e5cf6804cd9b08221444d4c9a
Willian Henkel <willianhenkel@gmail.com>
2024-01-28 00:15:54 -0300

    added cutscenes

--- Assets/Scripts/CutScene.cs
+++ Assets/Scripts/CutScene.cs
@@ -1,17 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CutScene : MonoBehaviour
 {
     public int screenPosition;
+    public int totalScreens;
+    public GameObject[] screens;
+    public GameObject fadeIn;
+    public GameObject fadeOut;
+    private GameObject actualScreen;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        Instantiate(fadeIn);
+        actualScreen = Instantiate(screens[screenPosition].gameObject);
+        screenPosition++;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             
+            Instantiate(fadeIn);
+            if (actualScreen != null)
+            {
+                Destroy(actualScreen);
+                Instantiate(fadeOut);
+            }
+            actualScreen = Instantiate(screens[screenPosition].gameObject);
+            
+            screenPosition++;
         }
     }
 }
